6e896979d3b4d5568280cfc31591f8a373d7dbe4
AkselDjun <akseldjun@gmail.com>
2021-01-16 15:30:15 +0300

    final project

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const express = require('express')
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    weatherRequest = require('./requests/weather.request')
 
 const app = express()
+// 0156e260df638481de99473702a31b97
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', { weather: null, error: null })
+})
+
+app.post('/', async (req, res) => {
+    const { city } = req.body
+
+    const { weather, error } = await weatherRequest(city)
+    res.render('index', { weather, error })
 })
 
 app.listen(3000, () => {
